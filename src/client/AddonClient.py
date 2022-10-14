f22466b20c1e690a1554548b86db3bad3bae0903
LUMASERV <info@lumaserv.com>
2022-10-14 15:07:33 +0000

    Updated Python Client

--- src/client/AddonClient.py
+++ src/client/AddonClient.py
@@ -12,89 +12,89 @@ class AddonClient:
 
 
     def request(self, method, path, params={}, body={}):
-        r = self.session.request(method, self.base_url + path, data=data, params=params)
+        r = self.session.request(method, self.base_url + path, data=body, params=params)
         if(r.status_code < 200 or (r.status_code >= 300 and r.status_code < 400):
             raise Exception("Status code is " + r.status_code + "!")
         return r.json()
 
 
     def create_ssl_certificate(self, body, query_params={}):
-        return self.request("POST", "/ssl/certificates", query_params, body);
+        return self.request("POST", "/ssl/certificates", query_params, body)
 
 
     def get_ssl_certificates(self, query_params={}):
-        return self.request("GET", "/ssl/certificates", query_params);
+        return self.request("GET", "/ssl/certificates", query_params)
 
 
     def get_plesk_license_types(self, query_params={}):
-        return self.request("GET", "/license/plesk-types", query_params);
+        return self.request("GET", "/license/plesk-types", query_params)
 
 
     def search(self, query_params={}):
-        return self.request("GET", "/search", query_params);
+        return self.request("GET", "/search", query_params)
 
 
     def get_ssl_certificate(self, id, query_params={}):
-        return self.request("GET", "/ssl/certificates/{id}".format(id=id), query_params);
+        return self.request("GET", "/ssl/certificates/{id}".format(id=id), query_params)
 
 
     def get_ssl_organisation(self, id, query_params={}):
-        return self.request("GET", "/ssl/organisations/{id}".format(id=id), query_params);
+        return self.request("GET", "/ssl/organisations/{id}".format(id=id), query_params)
 
 
     def delete_ssl_organisation(self, id, query_params={}):
-        return self.request("DELETE", "/ssl/organisations/{id}".format(id=id), query_params);
+        return self.request("DELETE", "/ssl/organisations/{id}".format(id=id), query_params)
 
 
     def create_ssl_contact(self, body, query_params={}):
-        return self.request("POST", "/ssl/contacts", query_params, body);
+        return self.request("POST", "/ssl/contacts", query_params, body)
 
 
     def get_ssl_contacts(self, query_params={}):
-        return self.request("GET", "/ssl/contacts", query_params);
+        return self.request("GET", "/ssl/contacts", query_params)
 
 
     def create_ssl_organisation(self, body, query_params={}):
-        return self.request("POST", "/ssl/organisations", query_params, body);
+        return self.request("POST", "/ssl/organisations", query_params, body)
 
 
     def get_ssl_organisations(self, query_params={}):
-        return self.request("GET", "/ssl/organisations", query_params);
+        return self.request("GET", "/ssl/organisations", query_params)
 
 
     def get_ssl_type(self, id, query_params={}):
-        return self.request("GET", "/ssl/types/{id}".format(id=id), query_params);
+        return self.request("GET", "/ssl/types/{id}".format(id=id), query_params)
 
 
     def get_ssl_contact(self, id, query_params={}):
-        return self.request("GET", "/ssl/contacts/{id}".format(id=id), query_params);
+        return self.request("GET", "/ssl/contacts/{id}".format(id=id), query_params)
 
 
     def delete_ssl_contact(self, id, query_params={}):
-        return self.request("DELETE", "/ssl/contacts/{id}".format(id=id), query_params);
+        return self.request("DELETE", "/ssl/contacts/{id}".format(id=id), query_params)
 
 
     def create_plesk_license(self, body, query_params={}):
-        return self.request("POST", "/licenses/plesk", query_params, body);
+        return self.request("POST", "/licenses/plesk", query_params, body)
 
 
     def get_plesk_licenses(self, query_params={}):
-        return self.request("GET", "/licenses/plesk", query_params);
+        return self.request("GET", "/licenses/plesk", query_params)
 
 
     def get_ssl_types(self, query_params={}):
-        return self.request("GET", "/ssl/types", query_params);
+        return self.request("GET", "/ssl/types", query_params)
 
 
     def get_plesk_license(self, id, query_params={}):
-        return self.request("GET", "/licenses/plesk/{id}".format(id=id), query_params);
+        return self.request("GET", "/licenses/plesk/{id}".format(id=id), query_params)
 
 
     def update_plesk_license(self, id, body, query_params={}):
-        return self.request("PUT", "/licenses/plesk/{id}".format(id=id), query_params, body);
+        return self.request("PUT", "/licenses/plesk/{id}".format(id=id), query_params, body)
 
 
     def get_plesk_license_type(self, id, query_params={}):
-        return self.request("GET", "/license/plesk-types/{id}".format(id=id), query_params);
+        return self.request("GET", "/license/plesk-types/{id}".format(id=id), query_params)
 
 
--- src/client/BillingClient.py
+++ src/client/BillingClient.py
@@ -12,173 +12,173 @@ class BillingClient:
 
 
     def request(self, method, path, params={}, body={}):
-        r = self.session.request(method, self.base_url + path, data=data, params=params)
+        r = self.session.request(method, self.base_url + path, data=body, params=params)
         if(r.status_code < 200 or (r.status_code >= 300 and r.status_code < 400):
             raise Exception("Status code is " + r.status_code + "!")
         return r.json()
 
 
     def get_payment_reminder(self, id, query_params={}):
-        return self.request("GET", "/payment-reminders/{id}".format(id=id), query_params);
+        return self.request("GET", "/payment-reminders/{id}".format(id=id), query_params)
 
 
     def update_payment_reminder(self, id, body, query_params={}):
-        return self.request("PUT", "/payment-reminders/{id}".format(id=id), query_params, body);
+        return self.request("PUT", "/payment-reminders/{id}".format(id=id), query_params, body)
 
 
     def create_debit_mandate(self, body, query_params={}):
-        return self.request("POST", "/debit-mandates", query_params, body);
+        return self.request("POST", "/debit-mandates", query_params, body)
 
 
     def get_debit_mandates(self, query_params={}):
-        return self.request("GET", "/debit-mandates", query_params);
+        return self.request("GET", "/debit-mandates", query_params)
 
 
     def get_invoice_file(self, id, query_params={}):
-        return self.request("GET", "/invoices/{id}/file".format(id=id), query_params);
+        return self.request("GET", "/invoices/{id}/file".format(id=id), query_params)
 
 
     def create_invoice_position(self, id, body, query_params={}):
-        return self.request("POST", "/invoices/{id}/positions".format(id=id), query_params, body);
+        return self.request("POST", "/invoices/{id}/positions".format(id=id), query_params, body)
 
 
     def get_invoice_positions(self, id, query_params={}):
-        return self.request("GET", "/invoices/{id}/positions".format(id=id), query_params);
+        return self.request("GET", "/invoices/{id}/positions".format(id=id), query_params)
 
 
     def get_billing_position(self, id, query_params={}):
-        return self.request("GET", "/billing-positions/{id}".format(id=id), query_params);
+        return self.request("GET", "/billing-positions/{id}".format(id=id), query_params)
 
 
     def delete_billing_position(self, id, query_params={}):
-        return self.request("DELETE", "/billing-positions/{id}".format(id=id), query_params);
+        return self.request("DELETE", "/billing-positions/{id}".format(id=id), query_params)
 
 
     def update_billing_position(self, id, body, query_params={}):
-        return self.request("PUT", "/billing-positions/{id}".format(id=id), query_params, body);
+        return self.request("PUT", "/billing-positions/{id}".format(id=id), query_params, body)
 
 
     def get_bank_transactions(self, query_params={}):
-        return self.request("GET", "/bank-transactions", query_params);
+        return self.request("GET", "/bank-transactions", query_params)
 
 
     def get_debit_mandate(self, id, query_params={}):
-        return self.request("GET", "/debit-mandates/{id}".format(id=id), query_params);
+        return self.request("GET", "/debit-mandates/{id}".format(id=id), query_params)
 
 
     def get_bank_transaction(self, id, query_params={}):
-        return self.request("GET", "/bank-transactions/{id}".format(id=id), query_params);
+        return self.request("GET", "/bank-transactions/{id}".format(id=id), query_params)
 
 
     def create_billing_position(self, body, query_params={}):
-        return self.request("POST", "/billing-positions", query_params, body);
+        return self.request("POST", "/billing-positions", query_params, body)
 
 
     def get_billing_positions(self, query_params={}):
-        return self.request("GET", "/billing-positions", query_params);
+        return self.request("GET", "/billing-positions", query_params)
 
 
     def create_customer(self, body, query_params={}):
-        return self.request("POST", "/customers", query_params, body);
+        return self.request("POST", "/customers", query_params, body)
 
 
     def get_customers(self, query_params={}):
-        return self.request("GET", "/customers", query_params);
+        return self.request("GET", "/customers", query_params)
 
 
     def get_invoice_position(self, invoice_id, id, query_params={}):
-        return self.request("GET", "/invoices/{invoice_id}/positions/{id}".format(invoice_id=invoice_id, id=id), query_params);
+        return self.request("GET", "/invoices/{invoice_id}/positions/{id}".format(invoice_id=invoice_id, id=id), query_params)
 
 
     def delete_invoice_position(self, invoice_id, id, query_params={}):
-        return self.request("DELETE", "/invoices/{invoice_id}/positions/{id}".format(invoice_id=invoice_id, id=id), query_params);
+        return self.request("DELETE", "/invoices/{invoice_id}/positions/{id}".format(invoice_id=invoice_id, id=id), query_params)
 
 
     def update_invoice_position(self, invoice_id, id, body, query_params={}):
-        return self.request("PUT", "/invoices/{invoice_id}/positions/{id}".format(invoice_id=invoice_id, id=id), query_params, body);
+        return self.request("PUT", "/invoices/{invoice_id}/positions/{id}".format(invoice_id=invoice_id, id=id), query_params, body)
 
 
     def create_service_contract(self, body, query_params={}):
-        return self.request("POST", "/service-contracts", query_params, body);
+        return self.request("POST", "/service-contracts", query_params, body)
 
 
     def get_service_contracts(self, query_params={}):
-        return self.request("GET", "/service-contracts", query_params);
+        return self.request("GET", "/service-contracts", query_params)
 
 
     def get_debits(self, query_params={}):
-        return self.request("GET", "/debits", query_params);
+        return self.request("GET", "/debits", query_params)
 
 
     def get_customer(self, id, query_params={}):
-        return self.request("GET", "/customers/{id}".format(id=id), query_params);
+        return self.request("GET", "/customers/{id}".format(id=id), query_params)
 
 
     def update_customer(self, id, body, query_params={}):
-        return self.request("PUT", "/customers/{id}".format(id=id), query_params, body);
+        return self.request("PUT", "/customers/{id}".format(id=id), query_params, body)
 
 
     def get_invoice(self, id, query_params={}):
-        return self.request("GET", "/invoices/{id}".format(id=id), query_params);
+        return self.request("GET", "/invoices/{id}".format(id=id), query_params)
 
 
     def delete_invoice(self, id, query_params={}):
-        return self.request("DELETE", "/invoices/{id}".format(id=id), query_params);
+        return self.request("DELETE", "/invoices/{id}".format(id=id), query_params)
 
 
     def update_invoice(self, id, body, query_params={}):
-        return self.request("PUT", "/invoices/{id}".format(id=id), query_params, body);
+        return self.request("PUT", "/invoices/{id}".format(id=id), query_params, body)
 
 
     def get_service_contract_position(self, contract_id, id, query_params={}):
-        return self.request("GET", "/service-contracts/{contract_id}/positions/{id}".format(contract_id=contract_id, id=id), query_params);
+        return self.request("GET", "/service-contracts/{contract_id}/positions/{id}".format(contract_id=contract_id, id=id), query_params)
 
 
     def delete_service_contract_position(self, contract_id, id, query_params={}):
-        return self.request("DELETE", "/service-contracts/{contract_id}/positions/{id}".format(contract_id=contract_id, id=id), query_params);
+        return self.request("DELETE", "/service-contracts/{contract_id}/positions/{id}".format(contract_id=contract_id, id=id), query_params)
 
 
     def update_service_contract_position(self, contract_id, id, body, query_params={}):
-        return self.request("PUT", "/service-contracts/{contract_id}/positions/{id}".format(contract_id=contract_id, id=id), query_params, body);
+        return self.request("PUT", "/service-contracts/{contract_id}/positions/{id}".format(contract_id=contract_id, id=id), query_params, body)
 
 
     def create_invoice(self, body, query_params={}):
-        return self.request("POST", "/invoices", query_params, body);
+        return self.request("POST", "/invoices", query_params, body)
 
 
     def get_invoices(self, query_params={}):
-        return self.request("GET", "/invoices", query_params);
+        return self.request("GET", "/invoices", query_params)
 
 
     def get_debit(self, id, query_params={}):
-        return self.request("GET", "/debits/{id}".format(id=id), query_params);
+        return self.request("GET", "/debits/{id}".format(id=id), query_params)
 
 
     def create_service_contract_position(self, contract_id, body, query_params={}):
-        return self.request("POST", "/service-contracts/{contract_id}/positions".format(contract_id=contract_id), query_params, body);
+        return self.request("POST", "/service-contracts/{contract_id}/positions".format(contract_id=contract_id), query_params, body)
 
 
     def get_service_contract_positions(self, contract_id, query_params={}):
-        return self.request("GET", "/service-contracts/{contract_id}/positions".format(contract_id=contract_id), query_params);
+        return self.request("GET", "/service-contracts/{contract_id}/positions".format(contract_id=contract_id), query_params)
 
 
     def get_service_contract(self, id, query_params={}):
-        return self.request("GET", "/service-contracts/{id}".format(id=id), query_params);
+        return self.request("GET", "/service-contracts/{id}".format(id=id), query_params)
 
 
     def delete_service_contract(self, id, query_params={}):
-        return self.request("DELETE", "/service-contracts/{id}".format(id=id), query_params);
+        return self.request("DELETE", "/service-contracts/{id}".format(id=id), query_params)
 
 
     def update_service_contract(self, id, body, query_params={}):
-        return self.request("PUT", "/service-contracts/{id}".format(id=id), query_params, body);
+        return self.request("PUT", "/service-contracts/{id}".format(id=id), query_params, body)
 
 
     def create_payment_reminder(self, body, query_params={}):
-        return self.request("POST", "/payment-reminders", query_params, body);
+        return self.request("POST", "/payment-reminders", query_params, body)
 
 
     def get_payment_reminders(self, query_params={}):
-        return self.request("GET", "/payment-reminders", query_params);
+        return self.request("GET", "/payment-reminders", query_params)
 
 
